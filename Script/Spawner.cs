32cb6a5d1d0147e67907f5ee28885397f6efa48d
EnricoAffuso <EnricoAffuso>
2020-02-14 18:05:37 +0100

    Primo push

---
+++ Script/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //SerializeField serve a rendere visibile la variabile nell'inspector
+    [SerializeField] GameObject cubePrefab;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Sono spawnato"+ gameObject.name, gameObject);
+        //Instantiate(cubePrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)) // 0  = Mouse sinistro
+        {
+            Instantiate(cubePrefab, new Vector3(Random.Range(-5,5), Random.Range(-5, 5)), cubePrefab.transform.rotation);
+        }
+    }
+}
